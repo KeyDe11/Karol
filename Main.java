875b43b3d8eced5a43c3c7580a9cab8c5c7e5340
Karol <karolkorzybski@hotmail.com>
2018-03-11 15:54:00 +0100

    nowy

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("bicz");
+        System.out.print("troche zmian");
 	// write your code here
     }
 }
