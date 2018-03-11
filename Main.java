1795c52ccf24be527ebc37b8a141913862857ce3
Karol <karolkorzybski@hotmail.com>
2018-03-11 14:44:33 +0100

    linijka 3

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("linijka 1");
+        System.out.print("linijka 3");
 	// write your code here
     }
 }
