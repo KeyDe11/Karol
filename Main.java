f556594312b2f71a543d330e5e3ebb381af306b4
Karol <karolkorzybski@hotmail.com>
2018-03-11 14:48:13 +0100

    4

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.print("linijka 3");
+        System.out.print("linijka 4");
 	// write your code here
     }
 }
